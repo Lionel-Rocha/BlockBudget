7aaece711079199e80e144a3c7ef70aec286593e
Matheus Gomes <matheus_mgc@yahoo.com.br>
2024-07-15 00:54:32 -0300

    update orcamento page

--- src/routes/Orcamento.js
+++ src/routes/Orcamento.js
@@ -1,66 +1,70 @@
-import './Orcamento.css';
+import "./Orcamento.css";
 import TableServ from "../components/Table/Serv";
-import TablePeca from '../components/Table/Peca';
-import { Button, IconButton } from '@mui/material';
-import { ArrowForwardRounded, FileDownloadRounded } from '@mui/icons-material';
+import TablePeca from "../components/Table/Peca";
+import { Button, IconButton } from "@mui/material";
+import { ArrowForwardRounded, FileDownloadRounded } from "@mui/icons-material";
 
 const Orcamento = () => {
-    return (
-        <div className='orcamento'>
-            <div className='titulo'>
-                <h1>ORÇAMENTO #XXXX</h1>
-            </div>
-            <div className='tableServ'>
-                <TableServ />
-            </div>
-            <div className='tablePeca'>
-                <TablePeca /> 
-            </div>
-            <div className='bottom'>
-                <div className='bottom-left'>                
-                    <Button sx={{
-                        borderRadius: 3, 
-                        fontFamily: '"Space Grotesk", sans-serif',
-                        fontSize: '1rem',
-                        fontWeight: 900,
-                        backgroundColor: "#D9D9D9", 
-                        color: "#274DB7", 
-                        ':hover': {backgroundColor: "#274DB7", color: "#D1D3E2" },
-                        height: "60px"
-                        }}
-                        size="large"
-                        variant='contained'
-                        endIcon={<FileDownloadRounded />}
-                    >
-                        DOWNLOAD
-                    </Button>
-                </div>                
-               
-                <div className='bottom-right'>
-                    <div className='displayTotal'>
-                        <div className='totalTexto'>
-                            <h2>TOTAL:</h2>
-                            <div className='displayTexto'>
-                                <p>R$ 9999.99</p>
-                            </div>
-                        </div>                        
-                    </div>
-                    <IconButton sx={{
-                        borderRadius: 3, 
-                        fontWeight: "bolder",
-                        backgroundColor: "#D9D9D9", 
-                        color: "#274DB7", 
-                        ':hover': {backgroundColor: "#274DB7", color: "#D1D3E2" },
-                        height: "60px",
-                        width: "70px"
-                        }} size="large"
-                    >
-                        <ArrowForwardRounded fontSize='inherit'/>
-                    </IconButton>
-                </div>
-            </div>
+  return (
+    <div className="orcamento">
+      <div className="titulo">
+        <h1>ORÇAMENTO #XXXX</h1>
+      </div>
+      <div className="table">
+        <div className="tableServ">
+          <TableServ />
         </div>
-    )
-}
+        <div className="tablePeca">
+          <TablePeca />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-left">
+          <Button
+            sx={{
+              borderRadius: 3,
+              fontFamily: '"Space Grotesk", sans-serif',
+              fontSize: "1rem",
+              fontWeight: 900,
+              backgroundColor: "#D9D9D9",
+              color: "#274DB7",
+              ":hover": { backgroundColor: "#274DB7", color: "#D1D3E2" },
+              height: "60px",
+            }}
+            size="large"
+            variant="contained"
+            endIcon={<FileDownloadRounded />}
+          >
+            DOWNLOAD
+          </Button>
+        </div>
+        <div className="bottom-right">
+          <div className="display">
+            <div className="textoTotal">
+                <h2>TOTAL:</h2>
+            </div>
+            <div className="displayTotal">
+                <p>R$ 9999.99</p>
+            </div>
+          </div>
+          <IconButton
+            sx={{
+              borderRadius: 3,
+              fontWeight: "bolder",
+              backgroundColor: "#D9D9D9",
+              color: "#274DB7",
+              ":hover": { backgroundColor: "#274DB7", color: "#D1D3E2" },
+              height: "60px",
+              width: "70px",
+            }}
+            size="large"
+          >
+            <ArrowForwardRounded fontSize="inherit" />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Orcamento;
